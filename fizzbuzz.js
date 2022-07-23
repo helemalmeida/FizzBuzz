65931a38b8484969b63203d709f0207feb5ea6b3
helemalmeida <helemsmalmeida@gmail.com>
2022-07-22 22:47:01 -0300

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,12 @@
 function fizzBuzz(entrada) {
-    if ((entrada % 3 == 0) && (entrada % 5 == 0))
-    return 'fizzBuzz';
-    else if (entrada % 3 == 0) {
-    return 'fizz';
-    }
-    else if (entrada % 5 == 0) {
-    return 'buzz';
-    }
-    else 
-    return entrada;
+    if ((entrada % 3 == 0) && (entrada % 5 == 0)) {
+        return 'fizzBuzz';
+    } else if (entrada % 3 == 0) {
+        return 'fizz';
+    } else if (entrada % 5 == 0) {
+        return 'buzz';
+    } else 
+        return entrada;
 }
 
 const resultado = fizzBuzz(15);
@@ -19,13 +17,13 @@ console.log(resultado);
 // while (i<=50) {
 
 // function fizzBuzz() {
-// if(i%3===0 && i%5===0){
+// if(i%3===0 && i%5===0) {
 // console.log("fizzBuzz");
-// }else if(i%3===0){
+// } else if(i%3===0) {
 // console.log("fizz");
-// }else if(i%5===0){
+// } else if(i%5===0) {
 // console.log("buzz");
-// }else{
+// } else {
 // console.log(i);
 // }
 
